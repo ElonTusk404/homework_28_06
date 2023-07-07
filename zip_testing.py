a0bdd8fe49b33e458e099ec0caa0ce371ad5ad5a
ElonTusk404 <igmaflappy@gmail.com>
2023-07-07 16:23:53 +0400

    Update zip_testing.py

--- zip_testing.py
+++ zip_testing.py
@@ -1,10 +1,8 @@
 def zip_testing(*iterables):
-    result = []
     minlen = min(len(item) for item in iterables)
     for i in range(minlen):
         items = tuple(item[i] for item in iterables)
-        result.append(items)
-    return result
+        yield items
 
 
 
