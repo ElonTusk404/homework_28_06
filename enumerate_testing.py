80e016c3c66f6d84c6af0185a74115ab67f50978
ElonTusk404 <igmaflappy@gmail.com>
2023-07-07 16:21:19 +0400

    Update enumerate_testing.py

--- enumerate_testing.py
+++ enumerate_testing.py
@@ -1,5 +1,3 @@
 def enumerate_testing(arg, start=0):
-    result = []
     for i in range(len(arg)):
-        result.append((i + start, arg[i]))
-    return result
+        yield (i + start, arg[i])
